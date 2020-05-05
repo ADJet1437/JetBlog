0ca81ae4b7042ec6c7ec869c72167118c9f23672
梁子杰 <liangzijie1437@gmail.com>
2020-05-05 18:27:33 +0200

    Simplify app.py, use manager module to run in debug mode

--- app.py
+++ app.py
@@ -1,39 +1,15 @@
-from datetime import datetime
-from functools import wraps
-
-from flask import Flask, render_template, redirect, request, redirect, url_for, jsonify, session
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-from flask_login import UserMixin, LoginManager, current_user, login_user, logout_user
 
 from common.libs.release_time import get_release_time
 
 # app 
 app = Flask(__name__)
 
-import os
-
-current_path = os.getcwd()
-db_path = current_path + '/db'
-
 # Config
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///{}/sqlite3-blog.db".format(db_path)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'my_secret'
+app.config.from_pyfile("config/base_setting.py")
 # app db
 db = SQLAlchemy(app)
-
-
-# class BlogPost(db.Model):
-#     """Class for a sqlite3 table model
-#     """
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(50))
-#     subtitle = db.Column(db.String(50))
-#     content = db.Column(db.Text)
-#     author = db.Column(db.String(30))
-#     post_date = db.Column(db.DateTime)
 
 
 class UrlManager(object):
@@ -58,8 +34,3 @@ class UrlManager(object):
 # Global template funtions
 app.add_template_global(UrlManager.buildUrl, "buildUrl")
 app.add_template_global(UrlManager.buildStaticUrl, "buildStaticUrl")
-
-
-if __name__ == "__main__":
-  #  db.create_all()
-   app.run(host='0.0.0.0', port='5000', debug=True)
--- manager.py
+++ manager.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 from route import app
+from flask_script import Server, Command, Manager
 
+manager = Manager(app)
+manager.add_command("runserver", Server(host="0.0.0.0", use_debugger=True, use_reloader=True))
 
+@Command
 def create_all():
     from app import db
     from common.db_models.blog_post import BlogPost
     from common.db_models.user import User
     db.create_all()
 
+manager.add_command("create_all", create_all)
 
 def main():
-    app.run(debug=True)
+    manager.run()
 
 if __name__ == "__main__":
     try:
