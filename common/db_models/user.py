92c2da1d555d42bd579ff63c1a64f634060f8181
梁子杰 <liangzijie1437@gmail.com>
2020-05-06 19:57:04 +0200

    Added more columns into user table

--- common/db_models/user.py
+++ common/db_models/user.py
@@ -1,6 +1,10 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(50))
     password = db.Column(db.String(50))
+    user_salt = db.Column(db.String(50))
+    password_salt = db.Column(db.String(100))
+    create_time = db.Column(db.DateTime, default=datetime.utcnow)
