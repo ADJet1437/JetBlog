cabc7ea39d248af67a81b3817086583f949eddb7
梁子杰 <liangzijie1437@gmail.com>
2020-05-06 19:59:33 +0200

    Added more info for cookies

--- config/base_setting.py
+++ config/base_setting.py
@@ -3,6 +3,9 @@ This file stores common configuration
 """
 
 # DB conf
-SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/sqlite3-blog.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:////Users/liangzijie/sqlite3-flask-blog.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'my_secret'
+
+AUTH_CODE = "algnaoilavbaoiehfappzienppoiejagnaljbaslgjalgk"
+AUTH_COOKIE_NAME = "bearer"
