2c1c8646bd01714a5f042c6d402220475eb6270f
梁子杰 <liangzijie1437@gmail.com>
2020-05-06 19:56:12 +0200

    Added ability to create admin user

--- manager.py
+++ manager.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from app import db
 from route import app
 from flask_script import Server, Command, Manager
 
@@ -7,12 +8,26 @@ manager.add_command("runserver", Server(host="0.0.0.0", use_debugger=True, use_r
 
 @Command
 def create_all():
-    from app import db
     from common.db_models.blog_post import BlogPost
     from common.db_models.user import User
     db.create_all()
 
+@Command
+def create_admin():
+    from common.db_models.user import User
+    from config.secret_setting import user_name, password, user_salt, password_salt
+
+    model_user = User()
+    model_user.user_name = user_name
+    model_user.password = password
+    model_user.user_salt = user_salt
+    model_user.password_salt = password_salt
+
+    db.session.add( model_user )
+    db.session.commit()
+
 manager.add_command("create_all", create_all)
+manager.add_command("create_admin", create_admin)
 
 def main():
     manager.run()
