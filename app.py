c811e2d5c17c80c87a7699fa5620f14f40f65a95
梁子杰 <liangzijie1437@gmail.com>
2020-06-13 17:30:19 +0200

    Removed code which not using

--- app.py
+++ app.py
@@ -10,27 +10,3 @@ app = Flask(__name__)
 app.config.from_pyfile("config/base_setting.py")
 # app db
 db = SQLAlchemy(app)
-
-
-class UrlManager(object):
-
-    @staticmethod
-    def buildUrl(path):
-        # TODO: move to conf file
-        domain_name = "http://0.0.0.0:5001"
-        return "%s%s" % (domain_name, path)
-
-    @staticmethod
-    def buildStaticUrl(path):
-        """
-
-        :param path:
-        :return:
-        """
-        release = get_release_time() 
-        path = "/static" + path + '?v=' + release
-        return UrlManager.buildUrl( path )
-
-# Global template funtions
-app.add_template_global(UrlManager.buildUrl, "buildUrl")
-app.add_template_global(UrlManager.buildStaticUrl, "buildStaticUrl")
